50e663d97a745e57a3de48f47646dca15db285b4
zahra moosavi <moosavi_z@gmail.com>
2024-09-28 10:57:55 +0330

    add: add CountOcuurenct function and read me

--- Algorithm/src/BasicChallange1/CountOccurency.ts
+++ Algorithm/src/BasicChallange1/CountOccurency.ts
@@ -1,6 +1,21 @@
-function CountOccurrencies (data : string){
-    const arrayOfCharacters = data.split
-    console.log(arrayOfCharacters);
+function CountOccurrencies (string : string,desiredLetter:string){
+    let counterOfDesiredLetter = 0
+    for(let i = 0; i < string.length ;i++  ) {
+        if(string[i] === desiredLetter ) 
+            counterOfDesiredLetter ++
+
+      
+    }
+    return counterOfDesiredLetter
+}
+
+
+
+const CountOccurrencies2 = (string : string,desiredLetter:string) => {
+    
+    console.log(string.split(desiredLetter));
+    
+    string.split(desiredLetter).length - 1;}
     
 
-}
+export default CountOccurrencies2
